41ec0bdb00a164f346832534d73fe91ab4b1c470
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2017-12-15 14:22:35 +0200

    lesson5CircleArea

--- src/main/java/com/kitcenter/app/classwork/lesson5/CircleArea.java
+++ src/main/java/com/kitcenter/app/classwork/lesson5/CircleArea.java
@@ -1,15 +1,25 @@
-package com.kitcenter.app.Classwork.lesson5;
+package com.kitcenter.app.classwork.lesson5;
 
 import java.util.Scanner;
 
 public class CircleArea {
     public static void calculate(){
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter circle radius");
         double radius = scanner.nextDouble();
         double circleArea = Math.PI * radius * radius;
         System.out.println("Area is " + circleArea);
+        System.out.println("Please enter circle radius");
         double radius2 = scanner.nextDouble();
         double circleArea2 = Math.PI * radius2 * radius2;
         System.out.println("Area is " + circleArea2);
+        calculateBigger(circleArea,circleArea2);
+    }
+
+    public static void calculateBigger(double area1,double area2){
+        if (area1 > area2)
+            System.out.print("Area1 is bigger");
+        else
+            System.out.print("Area2 is bigger");
     }
 }
--- src/main/java/com/kitcenter/app/classwork/runners/lesson5/CircleAreaRunners.java
+++ src/main/java/com/kitcenter/app/classwork/runners/lesson5/CircleAreaRunners.java
@@ -1,6 +1,6 @@
-package com.kitcenter.runners.classwork.lesson5;
+package com.kitcenter.app.classwork.runners.lesson5;
 
-import com.kitcenter.app.Classwork.lesson5.CircleArea;
+import com.kitcenter.app.classwork.lesson5.CircleArea;
 
 public class CircleAreaRunners {
     public static void main(String[] args) {
