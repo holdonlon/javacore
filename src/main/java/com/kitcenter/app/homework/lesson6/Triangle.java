e1714891484db7b79b3fc7bb86af20784242414f
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2017-12-16 12:55:10 +0200

    lesson6 triangle

---
+++ src/main/java/com/kitcenter/app/homework/lesson6/Triangle.java
@@ -0,0 +1,27 @@
+package com.kitcenter.app.homework.lesson6;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void check(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Pls enter A,B,C");
+        System.out.print("A:");
+        double sideA = scanner.nextDouble();
+        System.out.print("B:");
+        double sideB = scanner.nextDouble();
+        System.out.print("C:");
+        double sideC = scanner.nextDouble();
+        System.out.println("C^2:" +sideC*sideC);
+        System.out.println("B^2:" +sideB*sideB);
+        System.out.println("A^2:" +sideA*sideA);
+        boolean result= (sideC*sideC)==((sideA*sideA)+(sideB*sideB));
+        if (result){
+            System.out.println("Right angled triangle");
+        } else {
+            System.out.println("Not right angled triangle") ;
+        }
+
+    }
+}
---
+++ src/main/java/com/kitcenter/app/homework/runners/lesson6/Lessson6Runner.java
@@ -0,0 +1,10 @@
+package com.kitcenter.app.homework.runners.lesson6;
+
+import com.kitcenter.app.homework.lesson6.Triangle;
+
+public class Lessson6Runner {
+    public static void main(String[] args) {
+        Triangle.check();
+
+    }
+}
