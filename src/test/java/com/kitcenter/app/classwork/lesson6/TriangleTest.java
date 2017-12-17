01cfc9c7523decc2d417c04829a4c3427cf56537
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2017-12-17 20:13:12 +0200

    lesson7 parametrized tests

--- src/test/java/com/kitcenter/app/classwork/lesson6/TriangleTest.java
+++ src/test/java/com/kitcenter/app/classwork/lesson6/TriangleTest.java
@@ -1,61 +1,38 @@
 package com.kitcenter.app.classwork.lesson6;
 
 import com.kitcenter.app.homework.lesson6.Triangle;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+
+@RunWith(JUnitParamsRunner.class)
 public class TriangleTest {
     @Test
-    public void checktest1(){
-        boolean actResult = Triangle.checkSides(3,4,5);
-        Assert.assertEquals( true, actResult);
+    @Parameters({
+            "3, 4, 5",
+            "4, 5, 3",
+            "5, 4, 3",
+            "3, 5, 4",
+            "5, 12, 13",
+            "6, 8, 10",
+    })
+    public void checktestOK(double sideA, double sideB, double sideC) {
+        boolean actResult = Triangle.checkSides(sideA, sideB, sideC);
+        Assert.assertEquals(true, actResult);
     }
 
     @Test
-    public void checktest2() {
-        boolean actResult = Triangle.checkSides(4, 4, 4);
+    @Parameters({
+            "4, 4, 4",
+            "10, 15, 18",
+            "20, 18, 15",
+            "44, 58, 99",
+    })
+    public void checktestFail(double sideA, double sideB, double sideC) {
+        boolean actResult = Triangle.checkSides(sideA, sideB, sideC);
         Assert.assertEquals(false, actResult);
-    }
-    @Test
-    public void checktest3(){
-        boolean actResult = Triangle.checkSides(4,5,3);
-        Assert.assertEquals(true,actResult);
-    }
-
-    @Test
-    public void checktest4(){
-        boolean actResult = Triangle.checkSides(5,4,3);
-        Assert.assertEquals(true,actResult);
-    }
-
-    @Test
-    public void checktest5(){
-        boolean actResult = Triangle.checkSides(3,5,4);
-        Assert.assertEquals(true,actResult);
-    }
-    @Test
-    public void checktest6(){
-        boolean actResult = Triangle.checkSides(5,12,13);
-        Assert.assertEquals(true,actResult);
-    }
-    @Test
-    public void checktest7(){
-        boolean actResult = Triangle.checkSides(6,8,10);
-        Assert.assertEquals(true,actResult);
-    }
-    @Test
-    public void checktest8(){
-        boolean actResult = Triangle.checkSides(10,15,18);
-        Assert.assertEquals(false,actResult);
-    }
-    @Test
-    public void checktest9(){
-        boolean actResult = Triangle.checkSides(20,18,15);
-        Assert.assertEquals(false,actResult);
-    }
-    @Test
-    public void checktest10(){
-        boolean actResult = Triangle.checkSides(44,58,99);
-        Assert.assertEquals(false,actResult);
     }
 }
