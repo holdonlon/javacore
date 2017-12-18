57b7ab5dcb4ee18a63cd08719144ad5c4a936528
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2017-12-18 11:25:01 +0200

    lesson6 menu  Please enter the commit message for your changes. Lines starting

--- src/main/java/com/kitcenter/app/homework/lesson6/Menu.java
+++ src/main/java/com/kitcenter/app/homework/lesson6/Menu.java
@@ -9,16 +9,15 @@ public class Menu {
         System.out.println("To run Even or Odd input 2");
         System.out.println("To run What number bigger input 3");
         Scanner scanner = new Scanner(System.in);
-        double selection = scanner.nextDouble();
-        if (selection ==1){
-            Triangle.check();
-        }else if (selection==2){
-            EvenOrOdd.check();
-
-        }else if (selection==3){
-            WhatBiggerNumber.check();
-        }else{
-            System.out.println("Nothing to do here");
+        int selection = scanner.nextInt();
+        switch (selection){
+            case 1: Triangle.check();
+            break;
+            case 2: EvenOrOdd.check();
+            break;
+            case 3: WhatBiggerNumber.check();
+            break;
+            default: System.out.println("Nothing to do here");
         }
 
     }
